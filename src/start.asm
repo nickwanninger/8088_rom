a178d3b8d43af86767f628758422f3d969e5f8d7
Nick Wanninger <nickwanninger@gmail.com>
2019-10-14 17:56:54 -0500

    update print

--- src/start.asm
+++ src/start.asm
@@ -50,28 +50,42 @@ start:
 	jmp 0x0:0
 
 
-;; print a string stored at the address in DS:SI
+
+;; ----------------------------------------------------
+;; print
+;; Inputs:
+;;    cs:si - string pointer
+;; Outputs:
+;;    ax - number of bytes printed, -1 for error
+;;
+;; print the null terminated string pointed at by DS:SI
+;; ----------------------------------------------------
 print:
 	pushf ;; store the flags
-	push ax
 	push bx
+	push cx
 	push si
 	push ds
 	push cs
 	pop ds ;; ds = cs (so the string can be read from ROM)
 	       ;;          we reset this later, where we pop ds
 	cld ;; clear the direction flag for the lodsb instruction
-	;; use load string byte to walk the string
-.1:
+	mov bx, 0 ;; number of bytes written
+
+.print_next:
 	lodsb
-	or al,al
+	or al, al    ;; check if al is zero (null terminated)
 	jz .exit
 	out 0xFF, al ;; print to the special 0xFF port that is in the Emulator
-	jmp .1
+	inc cx       ;; increment counter
+	jmp .print_next
+
 .exit:
+	;; return the number of bytes written
+	mov ax, bx
 	pop	ds
 	pop	si
+	pop	cx
 	pop	bx
-	pop	ax
 	popf ;; pop the flags
 	ret
